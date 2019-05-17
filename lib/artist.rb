d4a5bda9ab56decf319c7f62387184562de84e61
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-05-17 14:42:27 -0700

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
+
+ # extend FancyDance::ClassMethods
+ #InstanceMethods
+  include Memorable::ClassMethods
+  include Paramable::ClassMethods
+  include Findable::ClassMethods
+
 class Artist
   attr_accessor :name
   attr_reader :songs
@@ -11,20 +18,12 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +35,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
